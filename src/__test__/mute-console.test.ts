70fe0841ff047247b65cd269fc4641f0d25f29b9
Konstantin Kraska <konstantin.kraska@smartclip.tv>
2023-04-21 17:23:26 +0200

    simplify mute console in tests

--- src/__test__/mute-console.test.ts
+++ src/__test__/mute-console.test.ts
@@ -8,7 +8,6 @@ it('should not invoke console methods when muteConsole is started', () => {
     const infoSpy = jest.spyOn(console, 'info')
     const debugSpy = jest.spyOn(console, 'debug')
 
-
     muteConsole(['Hi', (m) => m.includes('Message'), /Message/i]).start();
 
     console.log('Message');
@@ -27,8 +26,7 @@ it('should not invoke console methods when muteConsole is started', () => {
 it('should error to console when muteConsole is started', () => {
     const consoleSpy = jest.spyOn(console, 'error')
 
-    const mute = muteConsole(['Hi'],  { methods: ['log']});
-    mute.start();
+    muteConsole(['Hi'],  { methods: ['log']}).start();
 
     console.error('Hi');
 
@@ -39,8 +37,7 @@ it('should mute methods by a function', () => {
     const consoleSpy = jest.spyOn(console, 'log')
     const muteFn = (m: string) => m.includes('Hi')
 
-    const mute = muteConsole([muteFn],  { methods: ['log']});
-    mute.start();
+    muteConsole([muteFn],  { methods: ['log']}).start();
 
     console.log('Hi');
 
@@ -51,8 +48,7 @@ it('should mute methods by regular expressions', () => {
     const consoleSpy = jest.spyOn(console, 'log')
     const muteExpression = /Hi/i
 
-    const mute = muteConsole([muteExpression],  { methods: ['log']});
-    mute.start();
+    muteConsole([muteExpression],  { methods: ['log']}).start();
 
     console.log('Hi');
 
@@ -62,8 +58,7 @@ it('should mute methods by regular expressions', () => {
 it('should log to console when muteConsole is stopped', () => {
     const consoleSpy = jest.spyOn(console, 'log')
 
-    const mute = muteConsole(['Hi']);
-    mute.stop();
+    muteConsole(['Hi']).stop();
 
     console.log('Hi');
 
